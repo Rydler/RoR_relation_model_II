935eb6cedf382cbcd94f7fa06b57951dd46184f7
Daniel Morales <moralesaguilerad@gmail.com>
2020-08-13 00:23:38 -0400

    first commit to upload github

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  #resources :clients
   resources :pet_histories
-  resources :pets
+  #resources :pets
+
+  resources :clients do
+    resources :pets
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pets#index"
 end
